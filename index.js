e0d5374f899d2e9cb06f1093e9acfe7d01a0a5e8
sachinsachdev08 <sachinsachdev28@gmail.com>
2021-09-30 14:54:58 +0530

    Upated Project

--- index.js
+++ index.js
@@ -26,6 +26,23 @@ setInterval(showTime, 1000);
             min_.innerHTML = min < 10 ? "0" + min : min;
             sec_.innerHTML = sec < 10 ? "0" + sec : sec;
             am_pm_1.innerHTML = am_pm;
+
+
+            var status1 = document.querySelector('.item5');
+    if(am_pm === "AM" && hour >=4 && hour < 12){
+        status1.innerHTML = "Good Morning";
+    }
+    else if(am_pm === "PM" && hour >=1 && hour < 5){
+        status1.innerHTML = "Good Afternoon";
+        
+    }
+    else if(am_pm === "PM" && hour >=5 && hour < 8){
+        status1.innerHTML = "Good Evening";
+    }
+    else{
+        status1.innerHTML = "Good Night";
+
+}
   
           
 
@@ -33,16 +50,115 @@ setInterval(showTime, 1000);
         }
     showTime();
 
-var status1 = document.querySelector('.item5');
-if(am_pm === "AM" && 12 > hour_ >=4 ){
-    status1.innerHTML = "Good Morning";
+
+
+let party_1 = document.getElementById('button1');
+party_1.addEventListener("click",partyEnable);
+party_1.addEventListener("dblclick",partyDisable);
+
+let partyFlag = 0;
+function partyEnable() {
+    document.getElementById('home').src = "party2.svg"
+    document.getElementById('sentence').innerHTML = "Party time!!!";
+    document.getElementById('button1').innerHTML = "Double Click";
+    partyFlag = 1;
+
 }
-else if(am_pm === "PM" && 5 > hour_ >=12){
-    status1.innerHTML = "Good Afternoon";
+
+function partyDisable() {
+    document.getElementById('home').src = "maxresdefault.jpg"
+    document.getElementById('sentence').innerHTML = "";
+    document.getElementById('home').style.paddingTop = "0px";
+    document.getElementById('button1').innerHTML = "Party Time";
+    partyFlag = 0;
+
 }
-else if(am_pm === "PM" && 8 > hour_ >=5){
-    status1.innerHTML = "Good Afternoon";
+
+document.getElementById('drop1').addEventListener("change",Check);
+
+
+let startInterval1;
+let startInterval2;
+let startInterval3;
+let endInterval1;
+let endInterval2;
+let endInterval3;
+
+function Check() {
+    let wakeUptime = drop1.options[drop1.selectedIndex].value.split("-");
+
+
+    if(wakeUptime[0] === startInterval2 || wakeUptime[0] === startInterval3 ){
+        alert("Please Select a different range");
+        drop1.value = "null";
+        console.log('1')
+    }
+    else{
+        
+        startInterval1 = wakeUptime[0];
+        endInterval1 = wakeUptime [1];
+        console.log(startInterval1);
+        console.log(endInterval1)
+    }
 }
-else{
-    status1.innerHTML = "Good Night";
+
+
+document.getElementById('drop2').addEventListener("change",Check1);
+
+function Check1() {
+    let lunchTime  = drop2.options[drop2.selectedIndex].value.split("-");
+
+    if(lunchTime[0] === startInterval1 || lunchTime[0] === startInterval3 ){
+        alert("Please Select a different range");
+        drop2.value = "null";
+    }
+    else{
+        startInterval2 = lunchTime[0];
+        endInterval2 = lunchTime [1];
+    }
 }
+
+document.getElementById('drop3').addEventListener("change",Check2);
+
+function Check2() {
+    let napTime  = drop3.options[drop3.selectedIndex].value.split("-");
+
+    if(napTime[0] === startInterval2 || napTime[0] === startInterval3 ){
+        alert("Please Select a different range");
+        drop2.value = "null";
+    }
+    else{
+        startInterval3 = napTime[0];
+        endInterval3 = napTime [1];
+    }
+}
+
+let val1  = drop1.options[drop1.selectedIndex].value;
+let val2  = drop2.options[drop2.selectedIndex].value;
+let val3  = drop3.options[drop3.selectedIndex].value;
+let image = document.getElementById('home');
+let sentence = document.getElementById('sentence');
+function changeImg() {
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+
+    if(partyFlag === 0){
+        if(hour>=startInterval1 && hour<endInterval1){
+            image.src = "3.png";
+            sentence.innerHTML = "Wake Up..."
+        }
+        else if(hour>=startInterval2 && hour<endInterval2){
+            image.src = "home.png";
+            sentence.innerHTML = "Lets have some lunch..."
+         }
+        else if(hour>=startInterval3 && hour<endInterval3){
+            image.src = "2.png";
+            sentence.innerHTML = "Have a good sleep..."
+
+        }
+        
+    }
+}
+
+setInterval(changeImg,1000)
